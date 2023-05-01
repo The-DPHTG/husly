59282f622d6bf4c039d295bdb64c6f9b5c2ff110
Davlat Uralov <davlat.uralov@gmail.com>
2023-04-30 19:05:14 -0700

    added header n search bar to index, backend setup

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const path = require('path')
+var bodyParser = require('body-parser')
+
+const app = express();
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+ 
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
+app.use(express.static('public'))
+
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+  res.render(path.join(__dirname, './public/views/index.ejs'));
+});
+
+app.listen(8080, () => {
+  console.log('Listening on port ' + 8080);
+});
